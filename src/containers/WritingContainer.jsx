ec3f37417e5b5cb84778d1c8004ebbce2515fedd
StephanieZou1 <stephanie.zou3@gmail.com>
2020-07-07 19:20:36 -0400

    writing and resume

--- src/containers/WritingContainer.jsx
+++ src/containers/WritingContainer.jsx
@@ -22,27 +22,29 @@ const WritingContainer = () => {
             <Container>
                 <Row className="d-flex justify-content-center">
                 <CardDeck className="writing-deck">
-                <Card className="writing-card" >
-                <Card.Img variant="top" src={require("../images/nyu-purple.jpg")} />
-                <Card.Body>
-                    <Card.Title>Undergraduate Thesis Paper for BPE</Card.Title>
-                    <Card.Text>
-                    Zoning Ordinances and Policies in the 20th Century: <br/>The Entrenchment of Racial Segregation in Urban America
-                    </Card.Text>
-                    <Button variant="primary" onClick={onThesisClick}>See paper</Button>
-                </Card.Body>
-                </Card>
 
-                <Card className="writing-card">
-                <Card.Img variant="top" src={require("../images/medium.jpg")} />
-                <Card.Body>
-                    <Card.Title>Technical Publications on Medium</Card.Title>
-                    <Card.Text>
-                    Published writer for the Startup and Level Up Coding
-                    </Card.Text>
-                    <Button variant="primary" onClick={onPublicationClick}>See publications</Button>
-                </Card.Body>
-                </Card>
+                    <Card className="writing-card">
+                        <Card.Img variant="top" src={require("../images/medium.jpg")} />
+                        <Card.Body>
+                            <Card.Title>Technical Publications on Medium</Card.Title>
+                            <Card.Text>
+                            Published writer for the Startup and Level Up Coding
+                            </Card.Text>
+                            <Button variant="primary" onClick={onPublicationClick}>See publications</Button>
+                        </Card.Body>
+                    </Card>
+                    
+                    <Card className="writing-card" >
+                        <Card.Img variant="top" src={require("../images/nyu-purple.jpg")} />
+                        <Card.Body>
+                            <Card.Title>Undergraduate Thesis Paper for BPE</Card.Title>
+                            <Card.Text>
+                            Zoning Ordinances and Policies in the 20th Century: <br/>The Entrenchment of Racial Segregation in Urban America
+                            </Card.Text>
+                            <Button variant="primary" onClick={onThesisClick}>See paper</Button>
+                        </Card.Body>
+                    </Card>
+
                 </CardDeck>
                 </Row>
             </Container>
